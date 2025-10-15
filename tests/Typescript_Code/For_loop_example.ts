8d88a3aba2fa56990e0281b3f2ed15840e2859c8
prasadhebbar1993-prog <prasadhebbar1993@gmail.com>
2025-10-16 01:37:31 +0530

    Update code

---
+++ tests/Typescript_Code/For_loop_example.ts
@@ -0,0 +1,10 @@
+export {}; // 👈 Ensures this file is treated as a module (prevents variable redeclare errors)
+ 
+// program to display text 5 times
+const n: number = 5;
+ 
+// looping from i = 1 to 5
+for (let i: number = 1; i <= n; i++) {
+  console.log("I love Playwright.");
+}
+ 
